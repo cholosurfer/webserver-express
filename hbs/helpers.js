331b2204516a5457e4e7f672fe20ecf5eb8ef3df
Kabir <cholosurfer@gmail.com>
2020-12-27 20:44:41 -0600

    Primer commit

---
+++ hbs/helpers.js
@@ -0,0 +1,20 @@
+const hbs = require('hbs');
+
+// Helpers
+hbs.registerHelper( 'getYear', () => {
+
+    return new Date().getFullYear();
+
+});
+
+hbs.registerHelper( 'capitalize', ( text ) => {
+
+    let words = text.split(' ');
+
+    words.forEach( (word, i) => {
+        words[i] = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    });
+
+    return words.join(' ');
+
+});
